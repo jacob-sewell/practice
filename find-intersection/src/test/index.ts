d2a68c1f488331e96d603f6a9eead0d29dfe7ec2
Jacob Sewell <jasewell@gmail.com>
2022-10-07 13:10:35 -0500

    Find intersection: functionality for findIntersection(). Note this is using strict equality, which might or might not be the desired logic in a real use case.

--- find-intersection/src/test/index.ts
+++ find-intersection/src/test/index.ts
@@ -1,8 +1,25 @@
 import type { arrayIntersection } from './types';
 
 export default function findIntersection(
-    A: any[],
-    B: any[]
+    A: unknown[],
+    B: unknown[]
 ): arrayIntersection | null {
-    return [-1, -1];
+    const tail: unknown[] = [];
+    const listA: unknown[] = [...A];
+    const listB: unknown[] = [...B];
+    let lastA: unknown;
+    let lastB: unknown;
+
+    // Fill the tail array with every matching pair from the ends of the input lists
+    do {
+        lastA = listA.pop();
+        lastB = listB.pop();
+        if (lastA === lastB) tail.push(lastA);
+    } while (lastA === lastB);
+
+    // If tail is not empty, we calculate the indicies of the intersection.
+    // Otherwise, there were no matching pairs at the end and we return null.
+    return tail.length
+        ? [A.length - tail.length, B.length - tail.length]
+        : null;
 }
