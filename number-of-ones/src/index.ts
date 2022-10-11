a75fc490baf2fd268b9dd5e85a82ad26cd13d5e3
Jacob Sewell <jasewell@gmail.com>
2022-10-11 13:13:42 -0500

    Add numberOfOnesBelowExp() to calculate the easy plateau cases.

--- number-of-ones/src/index.ts
+++ number-of-ones/src/index.ts
@@ -1,4 +1,14 @@
 /**
+ * Recursively calculate the number of ones appearing in all nonnegative integers *less than* 10^exp.
+ * @param {number} exp The power of ten that forms the *non-inclusive* ceiling of the range
+ * @returns {number} the calculated number of 1s appearing in all nonnegative integers *less than* 10^exp
+ */
+function numberOfOnesBelowExp(exp: number): number {
+    if (exp <= 1) return 1;
+    return Math.pow(10, exp - 1) + 10 * numberOfOnesBelowExp(exp - 1);
+}
+
+/**
  * Calculate the number of ones appearing in all nonnegative integers less than *or equal to* n.
  * @param {number} n If a float is given, the fractional part will be truncated with Math.floor().
  * @returns {number}
