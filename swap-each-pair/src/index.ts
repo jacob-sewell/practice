58f610259b2d7b0c433e8ba71edb59d53ab3167a
Jacob Sewell <jasewell@gmail.com>
2022-10-11 17:49:34 -0500

    Implement swapPairs().

--- swap-each-pair/src/index.ts
+++ swap-each-pair/src/index.ts
@@ -1,3 +1,9 @@
 export default function swapPairs(inList: unknown[]): unknown[] {
-    return [...inList];
+    const outList = [...inList];
+    for (let i = 1; i < outList.length; i += 2) {
+        const t: unknown = outList[i];
+        outList[i] = outList[i - 1];
+        outList[i - 1] = t;
+    }
+    return outList;
 }
